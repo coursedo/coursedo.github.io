cb91c3752566d43e33fb88700d025948347cde8d
lminhtam <lminhtam99@gmail.com>
2021-01-10 14:45:29 +0700

    add course

--- src/core/epic.js
+++ src/core/epic.js
@@ -1,5 +1,6 @@
 import { combineEpics } from 'redux-observable'
 import { authEpics } from 'pages/SignIn/redux/epics'
 import { dashboardEpics } from 'pages/Dashboard/redux/epics'
+import { courseEpics } from 'pages/CreateCourse/redux/epics'
 
-export const rootEpic = combineEpics(authEpics, dashboardEpics)
+export const rootEpic = combineEpics(authEpics, dashboardEpics, courseEpics)
